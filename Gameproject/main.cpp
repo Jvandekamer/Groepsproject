60ef9fe0572e8bf98ffc9b46d9960076431b4f11
Jeff <jeff.vandekamer@student.hu.nl>
2020-04-02 15:33:05 +0200

    versie 3
    er moeten voor deze versie nog wijzigingen worden gemaakt aan de main, voor de hazzards, de devmode_functie kan weg.

--- Gameproject/main.cpp
+++ Gameproject/main.cpp
@@ -61,10 +61,18 @@ void game_Info(string logo_txt, string info_txt){
 }
 
 
-int Checklocation(int Playerlocation,vector<vector<int>>Map,int wumpus){
+int Checklocation(int Playerlocation,vector<vector<int>>Map,int wumpus,int Bat1, int Bat2,int traproom1,int traproom2){
 	if(Playerlocation==wumpus){
 		return 1;
 	}
+	else if(Playerlocation == Bat1 || Playerlocation == Bat2){
+		return 2;
+	}
+	else if(Playerlocation == traproom1 || Playerlocation == traproom2){
+		cout << "hij komt hier uit " << endl;
+		return 3;
+	}
+	
 // als er nieuwe elementen in het spel zet deze hier dan in met een else if statement
 	else{ // dit betekend dat de kamer leeg is
 		return 0;
@@ -72,12 +80,20 @@ int Checklocation(int Playerlocation,vector<vector<int>>Map,int wumpus){
 }
 
 
-string checksurrounding(int Playerlocation, vector<vector<int>>Map,int wumpus){
+string checksurrounding(int Playerlocation, vector<vector<int>>Map,int wumpus,int Bat1,int Bat2,int traproom1, int traproom2){
 	string Warning = "";
-	bool wumpdetected; // variable die laat zien of de wumpus is gespot
+	bool wumpdetected = false; // variable die laat zien of de wumpus is gespot
+	bool Batsdetected = false; // variabel dei alat zien of de bats zijn gesport
+	bool trapdetected = false; // variable dat laat zien dat er een val in de buurt zit
 	for(int i=0; i < Map[Playerlocation-1].size(); i++){ // hier check je voor de hazzards op 1 kamer afstand
 		if(Map[Playerlocation-1][i]==wumpus){
 			wumpdetected = true;
+		}
+		if(Map[Playerlocation-1][i]==Bat1 || Map[Playerlocation-1][i]==Bat2){
+			Batsdetected = true;
+		}
+		if(Map[Playerlocation-1][i]==traproom1 || Map[Playerlocation-1][i] == traproom2){
+			trapdetected = true;
 		}
 		// hier kan je de nieuwe hazzards toevoegen voor detectie
 	}
@@ -87,32 +103,49 @@ string checksurrounding(int Playerlocation, vector<vector<int>>Map,int wumpus){
 			if(Map[kamer-1][k] == wumpus){
 				wumpdetected = true;
 			}
+			if(Map[kamer-1][k] == Bat1 || Map[kamer-1][k] == Bat2){
+				Batsdetected = true;
+			}
+			if(Map[kamer-1][k] == traproom1 || Map[kamer-1][k] == traproom2){
+				trapdetected = true;
+			}
 			// hier kan je de neiuwe hazzards toevoegen voor detecie
 		}
 	}
 	if(wumpdetected == 1){
-		Warning+="U smell something nearby.\n";
+		Warning+="You smell something nearby.\n";
+	}
+	if(Batsdetected == 1){
+		Warning+="You can hear squeaks.\n";
+	}
+	if(trapdetected == 1){
+		Warning+="You feel a draft nearby.\n";
 	}
 	return Warning;
 }
 
 
-void Beweeg_speler(int & Playerlocation,vector<vector<int>> Map){
-	int wumpus = 8; // dit is de kamer waar de wumpus zich bevind.
+void Beweeg_speler(int & Playerlocation,vector<vector<int>> Map,int wumpus){
+	vector<int> bats{5,7}; // zorg ervoor dat deze bats mee krijgt
+	int bat1 = bats[0];
+	int bat2 = bats[1];
+	vector<int> Traprooms = {4,16};
+	int traproom1 = Traprooms[0];
+	int traproom2 = Traprooms[1];
 	while(true){
 		// Hier geven we de speler wat extra informatie over wat er zich in de kamers om hem heen bevint
-		string Warning = checksurrounding(Playerlocation,Map,wumpus);
+		string Warning = checksurrounding(Playerlocation,Map,wumpus,bat1,bat2,traproom1,traproom2);
 		if(Warning.size() > 0){
 			cout << Warning;
 			cout << endl;
 		}
-		cout << "U are currently in room: " << Playerlocation << endl;
+		cout << "You are currently in room: " << Playerlocation << endl;
 		cout << "Connected rooms are: ";
 		for(int i =0; i < Map[Playerlocation-1].size(); i++){
 			cout << Map[Playerlocation-1][i] << " ";
 		}
 		cout << endl;
-		cout << "What room would u like to go to? " << endl;
+		cout << "What room would you like to go to? " << endl;
 		int tmp = Playerlocation; //tijdelijke variable om de vorige positie te bewaren
 		cin >> Playerlocation;
 		while(true){ //hier gebruik ik de tijdelijke variable om te checken of de kamer die je invult wel  te bereiken is vanaf die positie
@@ -126,15 +159,34 @@ void Beweeg_speler(int & Playerlocation,vector<vector<int>> Map){
 				break;
 			}
 			cout << "That room cannot be reached from my current position" << endl; // als de ingegeven kamer niet overeen kwam met een van de mogelijkheden dan print hij deze melding
-			cout << "What room would u like to go to? " << endl; //en vraagt hij op nieuw om een locatie
+			cout << "What room would you like to go to? " << endl; //en vraagt hij op nieuw om een locatie
 			cin >> Playerlocation;
 		}
 		cout << endl;
 		//vanaf hier gaan we checken wat er in de huidge kamer zit, en wat daarvan het gevolg is
-		int inhoud_huidige_kamer = Checklocation(Playerlocation,Map,wumpus); //geeft nul als de kamer leeg is, geeft 1 als er een wumpus in de kamer zit. Als er nieuwe elenten in het spel komen voeg die hier aan toe met een nieuwe int
+		int inhoud_huidige_kamer = Checklocation(Playerlocation,Map,wumpus,bat1,bat2,traproom1,traproom2); //geeft nul als de kamer leeg is, geeft 1 als er een wumpus in de kamer zit. Als er nieuwe elenten in het spel komen voeg die hier aan toe met een nieuwe int
 		if(inhoud_huidige_kamer==1){
-			cout << "U Where killed by the wumpus" << endl;
+			cout << "You Where killed by the wumpus" << endl;
+			cout << "GAME OVER" << endl;
 			break; // deze breekt de loop en stopt dus het spel
+		}
+		else if(inhoud_huidige_kamer ==2){ // dit is wat er gebeurt als je in een bat kamer komt
+			cout << "You ran into an angry group of bats" << endl;
+			int lastposition = Playerlocation;
+			srand((unsigned) time(0));
+			Playerlocation = rand() % 20 + 1;
+			while(Playerlocation == wumpus || Playerlocation == lastposition){
+				Playerlocation = rand() % 20 + 1;
+			}
+			cout << "The angry bats picked you up and moved u to room: " << Playerlocation << endl;
+			cout << "The angry bats returned back to their room" << endl;
+			cout << endl;
+		}
+		else if(inhoud_huidige_kamer ==3){
+			cout << "The room u entered had no floor" << endl;
+			cout << "You fell down and died" << endl;
+			cout << "GAME OVER" << endl;
+			break; // breekt de loop en stopt het spel
 		}
 		else{ // als de waarde niet een van de bovenstaande is dan mag de speler verder met spelen
 			continue; 
@@ -143,17 +195,26 @@ void Beweeg_speler(int & Playerlocation,vector<vector<int>> Map){
 }
 
 
-void make_map_file (vector<vector<int>> map) {
+void make_map_file (vector<vector<int>> map, int wumpuslocation, vector<int> batslocation, vector<int> pitslocation) {
     ofstream myfile; 
     myfile.open("map.txt");
+
     for(unsigned int i=0; i<map.size(); i++){
-        myfile << i+1 << "; ";
+        myfile << "room " << i+1 << "; ";
         for(unsigned int j=0; j<map[i].size(); j++){
             myfile << map[i][j] << ", ";
         }
         myfile << "\n";
     }
-    myfile << "\n";
+    myfile << "\nbats: ";
+    for(unsigned int i=0; i<batslocation.size(); i++){
+        myfile << batslocation[i] << ", ";
+    }
+    myfile << "\npits: ";
+    for(unsigned int i=0; i<pitslocation.size(); i++){
+        myfile << pitslocation[i] << ", ";
+    }
+    myfile << "\nwumpus: " << wumpuslocation << "\n";
     myfile.close(); 
 }
 
@@ -168,6 +229,61 @@ vector<vector<int>> get_empty_map(int room_amount){
 }
 
 
+int read_wumpus_file() { 
+    string line; 
+    ifstream myfile ("map.txt"); 
+    if (myfile.is_open()){ 
+        while ( getline (myfile, line) ){
+            stringstream ss; 
+            ss << line; 
+            string temp; 
+            int found; 
+            if (line.find("wumpus") != std::string::npos) {
+                while (!ss.eof()) { 
+                    ss >> temp;
+                    if (stringstream(temp) >> found){
+                        myfile.close();
+                        return found;
+                    }
+                }
+            }
+        }
+    }
+    else{ 
+        cout << "Unable to open file";
+    }
+}
+
+
+vector<int> read_bats_file () { 
+    vector<int> bats;
+    string line; 
+    ifstream myfile ("map.txt"); 
+    if (myfile.is_open()){ 
+        while ( getline (myfile, line) ){
+            stringstream ss; 
+            ss << line; 
+            string temp; 
+            int found; 
+            if (line.find("bats") != std::string::npos) {
+                while (!ss.eof()) { 
+                    ss >> temp;
+                    if (stringstream(temp) >> found){
+                        bats.push_back(found);
+                    }
+                    temp = "";
+                }
+                myfile.close(); 
+                return bats;
+            }
+        }
+    }
+    else{ 
+        cout << "Unable to open file";
+    } 
+}
+
+
 vector<vector<int>> read_map_file () { 
     vector<vector<int>> map = get_empty_map(20);
     string line; 
@@ -178,15 +294,17 @@ vector<vector<int>> read_map_file () {
             stringstream ss; 
             ss << line; 
             string temp; 
-            int found; 
-            while (!ss.eof()) { 
-                ss >> temp;
-                if (stringstream(temp) >> found && found != index+1){
-                    map[index].push_back(found);
+            int found;
+            if (line.find("room") != std::string::npos) {
+                while (!ss.eof()) { 
+                    ss >> temp;
+                    if (stringstream(temp) >> found && found != index+1){
+                        map[index].push_back(found);
+                    }
+                    temp = "";
                 }
-                temp = "";
+                index++;
             }
-            index++;
         }
         myfile.close(); 
     }
@@ -235,8 +353,36 @@ vector<vector<int>> make_map(int room_amount){
 		map[i][k]++;
 		}
 	}
-	replay(map);
 	return map;
+}
+
+
+vector<int> read_pits_file () { 
+    vector<int> pits;
+    string line; 
+    ifstream myfile ("map.txt"); 
+    if (myfile.is_open()){ 
+        while ( getline (myfile, line) ){
+            stringstream ss; 
+            ss << line; 
+            string temp; 
+            int found; 
+            if (line.find("pits") != std::string::npos) {
+                while (!ss.eof()) { 
+                    ss >> temp;
+                    if (stringstream(temp) >> found){
+                        pits.push_back(found);
+                    }
+                    temp = "";
+                }
+                myfile.close(); 
+                return pits;
+            }
+        }
+    }
+    else{ 
+        cout << "Unable to open file";
+    } 
 }
 
 
@@ -246,20 +392,25 @@ vector<vector<int>> Dev_map(){ //Dit is de functie die er voor zorgt dat de gebr
 }
 
 
-vector<vector<int>>Gamemode(){ // Deze functie vraagt de gebruiker in welke mode hij/zij wil spelen en zorgt ervoor dat de bijbehorende map word geladen
+vector<vector<int>>Gamemode(int & wumpus, vector<int> & Bats){ // Deze functie vraagt de gebruiker in welke mode hij/zij wil spelen en zorgt ervoor dat de bijbehorende map word geladen
 	int gamemode;
 	cout << "Enter 1 for normal mode, or enter 2 for developer mode" << endl;
 	cin >> gamemode;
 	if(gamemode == 1){
+		srand((unsigned) time(0));
+		wumpus = rand() % 19 + 2;
 		return make_map(20); // hier roept hij de random map aan
 	}
+	wumpus = read_wumpus_file();
+	
 	return read_map_file(); // hier roept hij de developer map aan
+	
 }
 
 
 void printRoundInfo(vector<vector<int>> Map, string configuratie_txt){
     string answer;
-    cout << "Would you like to see this round info? (Y/N) " << endl;
+    cout << "Would you like to see the info for this round? (Y/N) " << endl;
     cin >> answer;
     if(answer == "Y" || answer == "y"){
         replay(Map);
@@ -272,7 +423,15 @@ void printRoundInfo(vector<vector<int>> Map, string configuratie_txt){
             while (getline(configuratie, setting_line)){
                 if (setting_line.find("wumpus") != string::npos){
                     start_index = setting_line.find("wumpus");
-                    cout << "De Wumpus zit in kamer: " << setting_line[start_index+7] << endl;
+                    cout << "The wumpus started in room: " << setting_line[start_index+9] << endl;
+                }
+                if (setting_line.find("bats") != string::npos){
+                    start_index = setting_line.find("bats");
+                    cout << "The bats were in the rooms: " << setting_line[start_index+7] << " and " << setting_line[start_index+10] << endl;
+                }
+                if (setting_line.find("pits") != string::npos){
+                    start_index = setting_line.find("pits");
+                    cout << "The pits were in the rooms: " << setting_line[start_index+7] << " and " << setting_line[start_index+10] << endl;
                 }
             }
         }
@@ -284,12 +443,15 @@ void printRoundInfo(vector<vector<int>> Map, string configuratie_txt){
 int main(){
 	game_Info("ASCII_Logo_Art.txt", "Game_Info.txt"); //print het startscherm en vraagt of je instructies wil
 	bool Nogmaalsspelen = true; //begint de loop waarin je meerdere rondes van het spel kan spelen
+	int wumpus;
+	vector<int>Bats;
+	vector<int>Pits;
 	while(Nogmaalsspelen==true){ // hier word het spel gestart
 		int Playerlocation = 1; //hier word de speler in het spel geplaatst deze is altijd hetzelfde
-		vector<vector<int>> Map = Gamemode(); //Hier word bepaald voor welke map er word gekozen
-		make_map_file(Map);
-		Beweeg_speler(Playerlocation,Map); //Hier word het spel gespeeld, met de map die word bepaald uit de gamemode functie
-//		printRoundInfo(Map,configbestand); (moet nog worden afgemaakt
+		vector<vector<int>> Map = Gamemode(wumpus,Bats); //Hier word bepaald voor welke map er word gekozen
+		make_map_file(Map,wumpus,Bats,Pits);
+		Beweeg_speler(Playerlocation,Map,wumpus,Bats); //Hier word het spel gespeeld, met de map die word bepaald uit de gamemode functie
+		printRoundInfo(Map,"map.txt");
 		cout << "Would u like to try again?" << endl; // vraagt of je nogmaals wil spelen
 		cin >> Nogmaalsspelen; // 1 betekend nog een ronde spelen, 0 zorgt ervoor dat je het spel afsluit
 	}
